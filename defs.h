b4696d84e26b75569cd4e9d1ea8a8350fc03c820
satish Rodd <rodd.satish@gmail.com>
2020-01-10 16:05:41 +0530

    Insert and delete

--- defs.h
+++ defs.h
@@ -16,11 +16,20 @@
 
 #include<stdbool.h>
 #include<stdio.h>
+#include<stdlib.h>
+//#include <netinet/in.h>
 
-#define ZERO        0
-#define ONE         1
-#define MAX_CHILDS  2
-#define DEBUG_ENABLED true
+#define ZERO            0
+#define ONE             1
+#define MAX_CHILDS      2
+#define DEBUG_ENABLED   true
+#define FIRST_BIT       0x80000000
+#define MAX_LEN         32
+
+typedef enum {
+    FAIL,
+    PASS
+} ret_types;
 
 #define PRINT_ERROR(format, ...) \
   printf("\n[%s:%d] ERROR:", __func__, __LINE__);\
@@ -32,14 +41,25 @@
       printf(format, ##__VA_ARGS__);\
   }\
 
+#define SHIFT_LEFT(prefix) (prefix = prefix<<1)
+
+#define GET_MASK(len) \
+  (0xffffffff <<(MAX_LEN-len))
+
 typedef struct node *node_ptr;
 
 struct node {
+    u_int32_t   prefix;
     node_ptr    children[MAX_CHILDS];
+    node_ptr    parent;
     bool        leaf;
+    u_int8_t    len;
 };
 
 int NODE_SIZE = sizeof(struct node);
 
-
+/*
+ * Explicit declaration of some functions.
+ */
+char *inet_ntoa(u_int32_t addr);
 #endif /* DEFS_H */
--- main.c
+++ main.c
@@ -12,8 +12,14 @@
  */
 
 #include"defs.h"
-#include<stdlib.h>
+#include <netinet/in.h>
 
+/*
+ * Allocate the memory for the node and
+ * initialize it.
+ *
+ * Return NULL upon failure, else pointer to memory.
+ */
 node_ptr 
 node_alloc() {
     node_ptr n = NULL;
@@ -24,16 +30,145 @@ node_alloc() {
         return(NULL);
     }
     //Initialize the node.
-    n->leaf = false;
-    n->children[ZERO] = NULL;
-    n->children[ONE]  = NULL;
+    n->leaf             = false;
+    n->children[ZERO]   = NULL;
+    n->children[ONE]    = NULL;
+    n->prefix           = 0;
+    n->len              = 0;
     PRINT_DEBUG("Success[%p]", n);
     return(n);
 }
 
+/*
+ * Insert the node in the prefix_tree
+ * return PASS upon success, FAIL upon failure.
+ */
+ret_types
+node_insert(node_ptr    root,
+            u_int32_t   prefix,
+            u_int32_t   mask) {
+    node_ptr curr   = root;
+    node_ptr new    = NULL;
+    u_int8_t index  = 0, depth = 0;
+    u_int32_t  tmp_prefix   = prefix;//, mask  = 0;
+
+    if ( !root ) {
+        PRINT_ERROR("Tree root is NULL");
+        return(FAIL);
+    }
+    
+    for (;mask;) {
+        depth++;
+        index = (prefix & FIRST_BIT)? 1:0;
+        /*
+         * If the child does not exist, but len is
+         * not exausted, we need to create the node.
+         */
+        if ( !curr->children[index] ) {
+            new = node_alloc(); 
+            if (!new) {
+                return(FAIL);
+            }
+            /*
+             * Every node created, will have the prefix
+             * from root to itself, stored in it, along with the
+             * depth from root, as len;
+             */
+            new->prefix = (tmp_prefix & GET_MASK(depth));
+            new->len    = depth;
+            new->parent = curr;
+            curr->children[index] = new;
+            curr = new;
+        } else {
+            curr = curr->children[index];
+        }
+        SHIFT_LEFT(prefix);
+        SHIFT_LEFT(mask);
+    }
+    /*
+     * Once the complete prefix walk is done, 
+     * then we must have found the node, and need to mark it 
+     * as leaf.
+     */
+    PRINT_DEBUG("Inserted prefix:%s\n", 
+                (char *)inet_ntoa(htonl(curr->prefix)));
+    curr->leaf = true;
+    return(PASS);
+}
+
+ret_types
+node_delete(node_ptr    root,
+            u_int32_t   prefix,
+            u_int32_t   mask) {
+    node_ptr curr   = root;
+    node_ptr parent = NULL;
+    u_int8_t index  = 0, depth = 0;
+    u_int32_t  tmp_prefix   = prefix;//, mask  = 0;
+
+    if ( !root ) {
+        PRINT_ERROR("Tree root is NULL");
+        return(FAIL);
+    }
+    
+    for (;mask;) {
+        index = (prefix & FIRST_BIT)? 1:0;
+        /*
+         * If the child does not exist, but len is
+         * not exausted, we need to create the node.
+         */
+        if ( !curr->children[index] ) {
+            PRINT_ERROR("Prefix not found\n");
+            return(PASS);
+        } else {
+            curr = curr->children[index];
+        }
+        SHIFT_LEFT(prefix);
+        SHIFT_LEFT(mask);
+    }
+    /*
+     * Clear leaf flag for node.
+     * Note: it can still be an intermediate node, for 
+     * a child.
+     */
+    curr->leaf = false;
+    parent = curr->parent;
+    /*
+     * Free the node memory.
+     * Also we need to free any intermediate nodes
+     * which are not leaves, or do not have any children.
+     */
+    while (!curr->leaf &&
+           !curr->children[ZERO] &&
+           !curr->children[ONE]) {
+           PRINT_DEBUG("Freeing node %p\n",curr); 
+           free(curr);
+           curr = parent;
+           curr->children[index] = NULL;
+           parent = parent->parent;
+    }
+    PRINT_DEBUG("deleted prefix:%s\n", 
+                (char *)inet_ntoa(htonl(tmp_prefix)));
+    return(PASS);
+}
+
+node_ptr
+tree_init() {
+    node_ptr n = NULL;
+    n = node_alloc();
+    if(n) {
+        PRINT_DEBUG("success");
+    }
+    return(n);
+}
+
 void main() {
-
-    node_ptr n1;
-    n1 = node_alloc();
-
+    node_ptr root = NULL;
+    root = tree_init();
+    node_insert(root, 0xa0000000, 0xf0000000); 
+    node_insert(root, 0xb0000000, 0xf0000000); 
+    node_insert(root, 0xc0000000, 0xf0000000); 
+    node_insert(root, 0x70000000, 0xf0000000); 
+    node_insert(root, 0x40000000, 0xf0000000); 
+    node_delete(root, 0xc0000000, 0xf0000000); 
+    node_delete(root, 0x70000000, 0xf0000000); 
 }
